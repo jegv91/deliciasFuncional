4678d6da2536b2d85f04a073e5868c70ce905a90
JEGV <jegv91@gmail.com>
2012-12-11 17:13:01 -0600

    cambios en el administrador

--- controllers/index.php
+++ controllers/index.php
@@ -1,18 +1,27 @@
 <?php
 	include_once('../models/categoria.php');
+	include_once('../models/usuario.php');
 	if(!isset($_SESSION)){
 		session_start();
 	}
-	
-	/*Cargar todas las categorias que esten en la base de datos*/
-	$categoria = new Categoria();
-	$usuario = "";
-	
-	if(isset($_SESSION['user_type'])){
-		if($_SESSION['user_type'] == 3){
-			$usuario = $_SESSION['user_id'];
+	/*Verifica si es la primera vez que se entro al sistema*/
+	$usuario = new Usuario();
+	$result = $usuario->busca_administrador(0);
+	/*Si no encontro ningun administrador, tiene que redirigirlo a la creacion de un nuevo admin*/
+	if ($result == 0){
+		/*Redirigimos al administrador a la vista para crear su usuario*/
+		header("location: ../controllers/registraAdministrador.php");
+	}else{		
+		/*Cargar todas las categorias que esten en la base de datos*/
+		$categoria = new Categoria();
+		$usuario = "";
+		
+		if(isset($_SESSION['user_type'])){
+			if($_SESSION['user_type'] == 3){
+				$usuario = $_SESSION['user_id'];
+			}
 		}
+		$lista = $categoria->busca_todos();
+		include_once("../views/index.php");
 	}
-	$lista = $categoria->busca_todos();
-	include_once("../views/index.php");
 ?>
