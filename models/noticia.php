a4a4372238208e3692931aa3ae4bf1d03878fb36
JEGV <jegv91@gmail.com>
2012-12-11 22:10:18 -0600

    cambios para el carrito

--- models/noticia.php
+++ models/noticia.php
@@ -24,5 +24,21 @@ class Noticia extends Active{
     public function __get($var){
 		return $this->$var;
     }
+	
+	public function busca_recientes() {
+		$tabla = get_class($this);
+		$sentencia = "SELECT * FROM $tabla ORDER BY fecha_creacion DESC LIMIT 3";
+		$arreglo = $this->sentencia($sentencia);
+		$numero = mysql_num_rows($arreglo);
+		if ($numero > 0) {
+			$resultado = array();
+			while ($fila = mysql_fetch_array($arreglo)) {
+				$resultado[] = $fila;
+			}
+			return $resultado;
+		} else {
+			return null; 
+		}
+    }
 }
 ?>
