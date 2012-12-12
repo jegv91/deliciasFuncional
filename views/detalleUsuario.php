995d98d4035351580a9e9ff531dcdc2b3a347abb
JEGV <jegv91@gmail.com>
2012-12-11 18:50:36 -0600

    Correcciones despliegue de informacion del usuario

--- views/detalleUsuario.php
+++ views/detalleUsuario.php
@@ -7,15 +7,22 @@ include_once("../views/header_index.php");
 		<h1>Informaci&oacute;n del usuario</h1>	
 			<div style="display:table-cell;">
 				<table class="table table-bordered">
-				<tr><th>Email</th>
-					<td><?php echo $usuario->email;?></td></tr>
-				<tr><th>Nombre</th><td><?php echo $usuario->nombre;?></td></tr>
-				<tr><th>Tipo</th><td><?php echo $usuario->tipo;?></td></tr>
-				<tr><th>Fecha de registro</th><td><?php echo $usuario->fecha_registro;?></td></tr>
+					<tr><th>Email</th>
+						<td><?php echo $usuario->email;?></td>
+					</tr>
+					<tr><th>Nombre</th>
+						<td><?php echo $usuario->nombre;?></td>
+					</tr>
+					<tr><th>Tipo</th>
+						<td><?php echo $usuario->tipo;?></td>
+					</tr>
+					<tr><th>Fecha de registro</th>
+						<td><?php echo $usuario->fecha_registro;?></td>
+					</tr>
+					<tr><th>Fotograf&iacute;a</th>
+						<td><?php echo "<img src=".$usuario->foto." style='max-width: 300px; max-height: 300px'>";?></td>
+					</tr>
 				</table>
-			</div>
-			<div style="display:table-cell;">
-				<?php echo "<img src=".$usuario->foto.">";?>
 			</div>	
 			<a class='btn btn-primary' onclick="history.back()" ><i class='icon-arrow-left icon-white'></i> Regresar</a>
 		</div>
